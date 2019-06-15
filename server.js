0dd545c91dabfc4608a405944378a0589ee37ae4
jzubak <jzubak81@gmail.com>
2019-06-15 12:35:05 -0400

    scraping the New Yorker successfully

--- server.js
+++ server.js
@@ -1,65 +1,44 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
 var mongojs = require("mongojs");
-var app = express();
+var express = require("express");
+var mongoose = require("mongoose");
 
-axios.get("http://www.cnn.com").then(function (response) {
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+//     mongoose.connect(MONGODB_URI);
 
-    // Load the HTML into cheerio
-    var $ = cheerio.load(response.data);
-
-    // Make an empty array for saving our scraped info
-    var results = [];
-
-
-    var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-    mongoose.connect(MONGODB_URI);
-
-
-    var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
 var db = require("./models");
 
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/NEWScrape", { useNewUrlParser: true });
 
 // Routes
-
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+  axios.get("https://www.newyorker.com/popular").then(function(response) {
     var $ = cheerio.load(response.data);
-
-    $("article h2").each(function(i, element) {
+    console.log("data" + $);
+    $("div.Card__mostPopularRiver___2N9m5 > div").each(function(i, element) {
       var result = {};
 
       result.title = $(this)
-        .children("a")
+        .find("h3")
+        .text();
+      result.desc = $(this)
+        .find("p")
         .text();
       result.link = $(this)
-        .children("a")
-        .attr("href");
-
+      .find("h3")
+      .parent()
+      .attr("href");
+    console.log(result);
 
       db.Article.create(result)
         .then(function(dbArticle) {
@@ -95,7 +74,6 @@ app.get("/articles/:id", function(req, res) {
   })
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   db.Note.create(req.body)
   .then(function(dbNote){
